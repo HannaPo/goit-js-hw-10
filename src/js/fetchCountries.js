9969dcba8b64fa09006f678c8bd2fad484d7d3db
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-03-26 13:05:38 +0300

    fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 export function fetchCountries(name) {
-    BASE_URL = 'https://restcountries.com/v3.1/name';
+   const BASE_URL = 'https://restcountries.com/v3.1/name';
     
     return fetch(`${BASE_URL}/${name}?fields=name,capital,flags,population,languages`)
         .then(response => {
